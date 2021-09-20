92627843287ad41102e6bbe4c41547a412dd0299
HiteshPatel10 <hpatel10@gmail.com>
2021-09-20 21:51:42 +0530

    Filter list created Firebase firestore dependency added

--- app/src/main/java/com/geekaid/collagenotes/ui/screens/FilterScreen.kt
+++ app/src/main/java/com/geekaid/collagenotes/ui/screens/FilterScreen.kt
@@ -6,35 +6,48 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.focus.focusModifier
 import androidx.compose.ui.unit.dp
 import com.geekaid.collagenotes.components.dropdownList
+import com.geekaid.collagenotes.util.*
 
 @Composable
 fun FilterScreen() {
-    val list = listOf("Item1", "Item2", "Item3")
+    listOf("Item1", "Item2", "Item3")
+
 
     var course by remember { mutableStateOf("") }
     var branch by remember { mutableStateOf("") }
     var year by remember { mutableStateOf("") }
     var semester by remember { mutableStateOf("") }
-    var subject by remember { mutableStateOf("") }
 
     Column(modifier = Modifier.padding(8.dp)) {
-        course = dropdownList(list = list, label = "Course")
-        dropdownList(list = list, label = "Branch")
-        dropdownList(list = list, label = "Year")
-        dropdownList(list = list, label = "Semester")
-        dropdownList(list = list, label = "Subject")
+
+        course = dropdownList(list = courseList, label = "Course")
+
+        when (course) {
+            "BTech" -> {
+                branch = dropdownList(list = branchList, label = "Branch")
+                year = dropdownList(list = yearList, label = "Year")
+                semester = dropdownList(list = semesterList, label = "Semester")
+            }
+        }
+
+        when (branch) {
+            "Computer Science" -> dropdownList(list = csSubjectList, label = "Subject")
+        }
+
+
+
         Button(
-            onClick = { /*TODO*/ },
+            onClick = {
+
+            },
             modifier = Modifier
                 .padding(start = 128.dp, end = 128.dp, top = 16.dp)
                 .fillMaxWidth()
         ) {
-            Text(text = course)
+            Text(text = "Submit")
         }
     }
 }
---
+++ app/src/main/java/com/geekaid/collagenotes/util/FilterList.kt
@@ -0,0 +1,24 @@
+package com.geekaid.collagenotes.util
+
+val courseList = listOf("BTech")
+
+val yearList = listOf("1 year", "2 year", "3 year", "4 year")
+
+val semesterList = listOf(
+    "1 semester",
+    "2 semester",
+    "3 semester",
+    "4 semester",
+    "5 semester",
+    "6 semester",
+    "7 semester",
+    "8 semester"
+)
+
+val branchList = listOf(
+    "Computer Science", "Electrical", "Mechanical", "Electronics", "Civil"
+)
+
+val csSubjectList = listOf(
+    "Operating System", "Object Oriented Programing", "Compiler Design", "Analysis Of Algorithm"
+)
