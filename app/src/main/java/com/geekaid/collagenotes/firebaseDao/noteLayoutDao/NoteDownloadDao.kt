94f2edeabd9fed7bb4368abc45c9019dce5f6a4a
HiteshPatel10 <hpatel10@gmail.com>
2021-11-26 22:38:21 +0530

    File cant be downloaded if already present in the storage

--- app/src/main/java/com/geekaid/collagenotes/firebaseDao/noteLayoutDao/NoteDownloadDao.kt
+++ app/src/main/java/com/geekaid/collagenotes/firebaseDao/noteLayoutDao/NoteDownloadDao.kt
@@ -1,15 +1,20 @@
 package com.geekaid.collagenotes.firebaseDao.noteLayoutDao
 
 import android.app.DownloadManager
+import android.content.Context
+import android.net.Uri
 import android.os.Environment
+import android.widget.Toast
 import com.geekaid.collagenotes.model.FileUploadModel
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.DocumentReference
 import com.google.firebase.firestore.FieldValue
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.storage.ktx.storage
+import java.io.File
 
-fun noteDownloadDao(note: FileUploadModel, downloadManager: DownloadManager) {
+fun noteDownloadDao(note: FileUploadModel, context: Context, downloadManager: DownloadManager) {
 
     val storage = Firebase.storage
     val currentUser = FirebaseAuth.getInstance().currentUser!!
@@ -25,22 +30,45 @@ fun noteDownloadDao(note: FileUploadModel, downloadManager: DownloadManager) {
     storage.reference.child("courses").child(note.course).child(note.branch).child(note.subject)
         .child("notes").child(note.fileInfo.fileUploadPath).downloadUrl
         .addOnSuccessListener { uri ->
+
             val index: Int = (note.fileInfo.fileMime.lastIndexOf('/')).plus(1)
+            val path =
+                Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS)
+            val fileName = "${note.fileInfo.fileName}.${note.fileInfo.fileMime.substring(index)}"
+            val file = "$path/$fileName"
 
-            db.runBatch { batch ->
-                batch.update(noteRef, "downloadedTimes", FieldValue.increment(1))
-                batch.update(favNoteRef, "downloadedTimes", FieldValue.increment(1))
+            if (!File(file).exists()) {
+                Toast.makeText(context, "Download started", Toast.LENGTH_LONG).show()
+                downloadFile(uri, note, noteRef, favNoteRef, fileName, downloadManager)
+            } else {
+                Toast.makeText(context, "File already exists", Toast.LENGTH_SHORT).show()
             }
-
-            val request = DownloadManager.Request(uri)
-                .setTitle(note.fileInfo.fileName)
-                .setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE)
-                .setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED)
-                .setAllowedOverMetered(true)
-                .setDestinationInExternalPublicDir(
-                    Environment.DIRECTORY_DOCUMENTS,
-                    "${note.fileInfo.fileName}.${note.fileInfo.fileMime.substring(index)}"
-                )
-            downloadManager.enqueue(request)
         }
+}
+
+fun downloadFile(
+    uri: Uri,
+    note: FileUploadModel,
+    noteRef: DocumentReference,
+    favNoteRef: DocumentReference,
+    fileName: String,
+    downloadManager: DownloadManager
+) {
+    val db = FirebaseFirestore.getInstance()
+
+    db.runBatch { batch ->
+        batch.update(noteRef, "downloadedTimes", FieldValue.increment(1))
+        batch.update(favNoteRef, "downloadedTimes", FieldValue.increment(1))
+    }
+
+    val request = DownloadManager.Request(uri)
+        .setTitle(note.fileInfo.fileName)
+        .setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE)
+        .setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED)
+        .setAllowedOverMetered(true)
+        .setDestinationInExternalPublicDir(
+            Environment.DIRECTORY_DOCUMENTS,
+            fileName
+        )
+    downloadManager.enqueue(request)
 }
