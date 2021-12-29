6cc5d6e85d9f185a8ea7ecde46de65d1068f02f7
HiteshPatel10 <hpatel10@gmail.com>
2021-12-29 19:34:04 +0530

    theme optimized

--- app/src/main/java/com/geekaid/collagenotes/ui/theme/Theme.kt
+++ app/src/main/java/com/geekaid/collagenotes/ui/theme/Theme.kt
@@ -2,35 +2,35 @@ package com.geekaid.collagenotes.ui.theme
 
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 
-private val DarkColorPalette = darkColors(
-    primary = Cyan800,
-    primaryVariant = Purple700,
-    secondary = Teal200
+private val DarkColorPalette = lightColors(
+    primary = Blue600,
+    onPrimary = Color.Black,
+    primaryVariant = Blue600,
+    secondary = Color.White,
+    secondaryVariant = Teal300,
+    error = RedErrorDark,
+    onError = RedErrorLight
 )
 
 private val LightColorPalette = lightColors(
-    primary = Cyan800,
-    primaryVariant = Purple700,
-    secondary = Teal200
-
-    /* Other default colors to override
-    background = Color.White,
-    surface = Color.White,
-    onPrimary = Color.White,
+    primary = Blue600,
+    primaryVariant = Blue400,
+    onPrimary = Color.Black,
+    secondary = Color.White,
+    secondaryVariant = Teal300,
     onSecondary = Color.Black,
-    onBackground = Color.Black,
-    onSurface = Color.Black,
-    */
+    error = RedErrorDark,
+    onError = RedErrorLight
 )
 
 @Composable
 fun CollageNotesTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    content: @Composable() () -> Unit
+    content: @Composable () -> Unit
 ) {
     val colors = if (darkTheme) {
         DarkColorPalette
